e600eec07c1ae2eee3446d7b46ea57ee1c334dbf
Daniel <55189660+DaninyanJo@users.noreply.github.com>
2020-09-18 11:10:28 +0300

    Fix
    fix listener

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function shuffle(str) {
       const li = document.createElement("li");
       li.innerText = element;
       output.appendChild(li);
-      li.addEventListener('mouseover', () => cheker())
+      li.addEventListener('mouseout', () => cheker())
     });
   })()
   function cheker() {
